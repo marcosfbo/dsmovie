1824f6c9548f57a41055e5d5f1e070c2d76a900d
Marcos Fernando <marcosfbo@gmail.com>
2022-01-14 21:12:16 -0300

    useParams

--- frontend/src/componentes/MovieCard/index.tsx
+++ frontend/src/componentes/MovieCard/index.tsx
@@ -6,15 +6,7 @@ type Props = {
     movie: Movie;
 };
 
-function MovieCard() {
-
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+function MovieCard( { movie } : Props) { //movie é do tipo Props.
 
     return (
         <div>
